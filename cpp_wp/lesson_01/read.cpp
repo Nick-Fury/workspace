e7ecbc10157df6097495d8121e2585651a4fc153
lixing <lx@ubuntu.(none)>
2015-10-13 01:37:30 -0700

    hello

---
+++ cpp_wp/lesson_01/read.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, const char *argv[])
+{
+	int num;
+
+	cout<<"Hello World !"<<endl;
+
+	cin >> num;
+	cout << "num is " << num <<endl;
+	return 0;
+}
